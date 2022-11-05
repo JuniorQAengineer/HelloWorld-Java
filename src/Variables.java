4248d093bb58c80548783c0014622cbf35d61982
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2022-11-05 16:01:11 +0300

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte num0 = 100;
+        int num1 = 543;
+        short num2 = 324;
+        long num3 = 2343255;
+
+        double num4 = 23.243432;
+        float num5 = 5.23f;
+
+        char abc = 'a';
+        boolean isTrue = true;
+        System.out.println(num0);
+    }
+}
