ec351dd9cf7b014d4811e5f4174f2aef8d50037b
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2022-11-06 08:38:33 +0300

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello";
+        System.out.println(s);
+        String space = " ";
+        String name = "Jack";
+        System.out.println(s+space+name);
+        System.out.println("My number is "+x);
+    }
+}
